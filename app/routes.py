9391f492954310cc5ed154931310ff7f9ebb085b
Brian <bstanton773@gmail.com>
2023-10-24 11:11:15 -0500

    Add templates for our routes

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,32 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'This works even after refactoring :)'
+    return render_template('index.html')
 
 @app.route('/favorite')
 def fav_five():
-    return 'This is the favorites endpoint!'
+    fav_colors = [
+        {
+            'color': 'blue',
+            'bs_color': 'primary'
+        },
+        {
+            'color': 'green',
+            'bs_color': 'success'
+        },
+        {
+            'color': 'red',
+            'bs_color': 'danger'
+        },
+        {
+            'color': 'yellow',
+            'bs_color': 'warning'
+        },
+        {
+            'color': 'teal',
+            'bs_color': 'info'
+        },
+    ]
+    return render_template('colors.html', favorites=fav_colors)
